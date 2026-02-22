7ea7fa5a61fa30ed607003a0a1f41ea9d12da4fb
mikedecr <mgdecrescenzo@gmail.com>
2026-02-22 11:17:25 -0600

    game-name enum

---
+++ src/linkedin_game_data/game_name.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+
+class GameName(Enum):
+    Tango = "Tango"
+    Queens = "Queens"
+    Pinpoint = "Pinpoint"
+    Crossclimb = "Crossclimb"
+    Zip = "Zip"
+    Mini_Sudoku = "Mini Sudoku"
